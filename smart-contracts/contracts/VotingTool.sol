ef55c650b55fb880d0026efbad3c3520903159b1
Thiago Cerutti <thiagocerutti@gmail.com>
2022-11-16 21:34:19 -0300

    refactoring + tests on smart contract

--- smart-contracts/contracts/VotingTool.sol
+++ smart-contracts/contracts/VotingTool.sol
@@ -2,46 +2,71 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract VotingTool {
-  mapping(address => bool) hasVoted;
-  mapping(address => uint256) votes;
-  mapping(address => bool) registeredCandidates;
-  address[] candidates;
-  address _owner;
-  bool isSet = false;
+    mapping(address => bool) hasVoted;
+    mapping(address => uint256) votes;
+    mapping(address => bool) registeredCandidates;
+    address[] candidates;
+    address[] voters;
+    address _owner;
 
-  constructor() {
-      _owner = msg.sender;
-  }
+    constructor() {
+        _owner = msg.sender;
+    }
 
-  function vote(address candidate) public {
-      require(!hasVoted[msg.sender], "account already voted");
-      require(registeredCandidates[candidate], "invalid candidate");
+    function vote(address candidate) public {
+        require(!hasVoted[msg.sender], "account already voted");
+        require(registeredCandidates[candidate], "invalid candidate");
 
-      votes[candidate]++;
-      hasVoted[msg.sender] = true;
-  }
+        votes[candidate]++;
+        hasVoted[msg.sender] = true;
+        voters.push(msg.sender);
+    }
 
-  function registerCandidate(address candidate) public {
-      require(candidate != _owner, "contract owner cannot be candidate");
-      require(
-          !registeredCandidates[candidate],
-          "candidate already registered"
-      );
+    function registerCandidate(address candidate) public {
+        require(candidate != _owner, "contract owner cannot be candidate");
+        require(
+            !registeredCandidates[candidate],
+            "candidate already registered"
+        );
 
-      registeredCandidates[candidate] = true;
-      candidates.push(candidate);
-  }
+        registeredCandidates[candidate] = true;
+        candidates.push(candidate);
+    }
 
-  function getVotes(address candidate) public view returns (uint256) {
-      require(
-          registeredCandidates[candidate],
-          "address is not a registered candidate"
-      );
+    function getVotes(address candidate) public view returns (uint256) {
+        require(
+            registeredCandidates[candidate],
+            "address is not a registered candidate"
+        );
 
-      return votes[candidate];
-  }
+        return votes[candidate];
+    }
 
-  function getAllCandidates() public view returns (address[] memory) {
-      return candidates;
-  }
+    function getAllCandidates() public view returns (address[] memory) {
+        return candidates;
+    }
+
+    event DataResetByOwner(uint256 timestamp);
+
+    function resetVoting() public {
+        resetVotes();
+        resetCandidates();
+        emit DataResetByOwner(block.timestamp);
+    }
+
+    function resetVotes() private {
+        for (uint256 i = 0; i < candidates.length; i++) {
+            votes[candidates[i]] = 0;
+        }
+        for (uint256 i = 0; i < voters.length; i++) {
+            hasVoted[voters[i]] = false;
+        }
+    }
+
+    function resetCandidates() private {
+        for (uint256 i = 0; i < candidates.length; i++) {
+            registeredCandidates[candidates[i]] = false;
+        }
+        delete candidates;
+    }
 }
--- smart-contracts/test/voting_tool.js
+++ smart-contracts/test/voting_tool.js
@@ -9,80 +9,93 @@ contract("VotingTool", async (accounts) => {
   let candidate1 = accounts[1];
   let candidate2 = accounts[2];
   let candidate3 = accounts[3];
+  let candidate4 = accounts[4];
   let contract;
 
   beforeEach(async () => {
     contract = await VotingTool.deployed();
-  })
+  });
 
-  describe('on startup', () => {
-    it('should have no candidates', async () => {
-      let actual = await contract.getAllCandidates()
-      assert.equal(0, actual.length)
-    })
-  })
+  describe("on startup", () => {
+    it("should have no candidates", async () => {
+      let actual = await contract.getAllCandidates();
+      assert.equal(0, actual.length);
+    });
+  });
 
-  describe('before register a candidate', () => {
-    it('should not accept contract owner as candidate', async () => {
+  describe("before register a candidate", () => {
+    it("should not accept contract owner as candidate", async () => {
       let contractOwner = accounts[0];
       try {
-        await contract.registerCandidate(contractOwner)
+        await contract.registerCandidate(contractOwner);
       } catch (error) {
-        assert.equal('contract owner cannot be candidate', error.data.reason)
+        assert.equal("contract owner cannot be candidate", error.data.reason);
         return;
       }
-      assert.fail('Expected throw not received');
-    })
+      assert.fail("Expected throw not received");
+    });
 
-    it('should not accept candidate already registered', async () => {
-      await contract.registerCandidate(candidate3)
+    it("should not accept candidate already registered", async () => {
+      await contract.registerCandidate(candidate3);
       try {
-        await contract.registerCandidate(candidate3)
+        await contract.registerCandidate(candidate3);
       } catch (error) {
-        assert.equal('candidate already registered', error.data.reason)
+        assert.equal("candidate already registered", error.data.reason);
         return;
       }
-      assert.fail('Expected throw not received');
-    })
-  })
+      assert.fail("Expected throw not received");
+    });
+  });
 
-  describe('after add a candidate', () => {
-    it('should be present on the candidate list', async () => {
-      await contract.registerCandidate(candidate1)
-      let actual = await contract.getAllCandidates()
-      assert.equal(candidate1, actual.find(element => element === candidate1))
-    })
+  describe("after add a candidate", () => {
+    it("should be present on the candidate list", async () => {
+      await contract.registerCandidate(candidate1);
+      let actual = await contract.getAllCandidates();
+      assert.equal(
+        candidate1,
+        actual.find((element) => element === candidate1)
+      );
+    });
 
-    describe('and the candidate receives a vote', () => {
-      it('should count 1 vote to the candidate', async () => {
-        await contract.vote(candidate1)
+    describe("and the candidate receives a vote", () => {
+      it("should count 1 vote to the candidate", async () => {
+        await contract.vote(candidate1);
         let actual = await contract.getVotes(candidate1);
-        assert.equal(1, actual)
-      })
+        assert.equal(1, actual);
+      });
 
-      it('should revert when double vote on the same candidate', async () => {
-        await contract.registerCandidate(candidate2)
-        await contract.vote(candidate2, { from: accounts[3] })
+      it("should revert when double vote on the same candidate", async () => {
+        await contract.registerCandidate(candidate2);
+        await contract.vote(candidate2, { from: accounts[3] });
         try {
-          await contract.vote(candidate2, { from: accounts[3] })
+          await contract.vote(candidate2, { from: accounts[3] });
         } catch (error) {
-          assert.equal('account already voted', error.data.reason)
+          assert.equal("account already voted", error.data.reason);
           return;
         }
-        assert.fail('Expected throw not received');
-      })
-      
-      it('should revert when voting in an inexistent candidate', async () => {
-        let invalidCandidate = accounts[4]
-        try {
+        assert.fail("Expected throw not received");
+      });
 
-          await contract.vote(invalidCandidate, { from: accounts[4] })
+      it("should revert when voting in an inexistent candidate", async () => {
+        let invalidCandidate = accounts[4];
+        try {
+          await contract.vote(invalidCandidate, { from: accounts[4] });
         } catch (error) {
-          assert.equal('invalid candidate', error.data.reason)
+          assert.equal("invalid candidate", error.data.reason);
           return;
         }
-        assert.fail('Expected throw not received');
-      })
-    })
-  })
+        assert.fail("Expected throw not received");
+      });
+    });
+  });
+
+  describe("after reset data", () => {
+    it("should reset votes emmiting 'DataResetByOwner' event", async () => {
+      await contract.registerCandidate(candidate4);
+      await contract.vote(candidate4, { from: accounts[5] });
+      let receipt = await contract.resetVoting();
+      let actual = receipt.logs[0].event;
+      assert.equal('DataResetByOwner', actual);
+    });
+  });
 });
