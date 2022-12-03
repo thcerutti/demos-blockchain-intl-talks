00143722ed1eeccb98f640b151821ea92a6aae45
Thiago Cerutti <thiagocerutti@gmail.com>
2022-12-03 10:18:38 -0300

    test: add tests to SalesRegistration tontract

--- smart-contracts/contracts/SalesRegistration.sol
+++ smart-contracts/contracts/SalesRegistration.sol
@@ -11,15 +11,15 @@ contract SalesRegistration {
     uint Timestamp;
   }
 
-  event ContractRegistered(address from, address to, uint timestamp);
+  event NegotiationRegistered(address from, address to, uint timestamp);
 
   function registerNegotiation(address from, address to, string memory contractHash) public {
     require(msg.sender == _owner, "only contract owner can reegister negotiations");
-    require(registeredNegotiations[contractHash].From != address(0), "negotiation already registered");
+    require(registeredNegotiations[contractHash].From == address(0), "negotiation already registered");
     
     registeredNegotiations[contractHash] = NegotiationData(from, to, block.timestamp);
     
-    emit ContractRegistered(from, to, block.timestamp);
+    emit NegotiationRegistered(from, to, block.timestamp);
   }
 
   function getContractDetails(string memory contractHash) public view returns (NegotiationData memory) {
--- smart-contracts/test/sales_registration.js
+++ smart-contracts/test/sales_registration.js
@@ -34,19 +34,90 @@ contract("SalesRegistration", function (accounts) {
   });
 
   describe("when registering a negotiation", () => {
-    it("should revert when sender is not owner", async () => {
-      try {
-        await contract.registerNegotiation(
+    describe("on pre-validating input", () => {
+      it("should revert when sender is not owner", async () => {
+        try {
+          await contract.registerNegotiation(
+            accounts[1],
+            accounts[2],
+            "somehash",
+            { from: accounts[3] }
+          );
+        } catch (error) {
+          assert.equal("revert", error.data.message);
+          assert.equal(
+            "only contract owner can reegister negotiations",
+            error.data.reason
+          );
+        }
+      });
+
+      it("should revert is hash already registered", async () => {
+        let myHash = new Date().toISOString();
+
+        const registerContractTestCase = async (hash) =>
+          await contract.registerNegotiation(accounts[1], accounts[2], hash);
+
+        await registerContractTestCase(myHash);
+        try {
+          await registerContractTestCase(myHash);
+        } catch (error) {
+          assert.equal("revert", error.data.message);
+          assert.equal("negotiation already registered", error.data.reason);
+          return;
+        }
+        assert.fail("registration not reverted as expected");
+      });
+    });
+
+    describe("on registering a negotiation", () => {
+      let negotiationHash;
+      beforeEach(() => {
+        negotiationHash = new Date().toISOString();
+      });
+
+      it('should emit "NegotiationRegistered" event', async () => {
+        let receipt = await contract.registerNegotiation(
           accounts[1],
           accounts[2],
-          "somehash",
-          { from: accounts[3] }
+          negotiationHash
         );
-      } catch (error) {
-        console.log(error.reason);
-        assert.equal("revert", error.data.message);
-        assert.equal("only contract owner can reegister negotiations", error.data.reason);
-      }
+        let { event, args } = receipt.logs[0];
+        assert.equal("NegotiationRegistered", event, "unexpected event name");
+        assert.equal(
+          accounts[1],
+          args.from,
+          "unexpected seller (from) account ID"
+        );
+        assert.equal(accounts[2], args.to, "unexpected buyer (to) account ID");
+      });
+    });
+
+    describe("after register a negotiation", () => {
+      const registerContract = async (from, to, hash) =>
+        await contract.registerNegotiation(from, to, hash);
+
+      it("should get the negotiation details from contract method", async () => {
+        let fromAddress = accounts[1];
+        let toAddress = accounts[2];
+        let negotiationHash = new Date().toISOString();
+
+        await registerContract(fromAddress, toAddress, negotiationHash);
+
+        let contractDetails = await contract.getContractDetails(
+          negotiationHash
+        );
+        assert.equal(
+          fromAddress,
+          contractDetails.From,
+          "unexpected origin account"
+        );
+        assert.equal(
+          toAddress,
+          contractDetails.To,
+          "unexpected destination account"
+        );
+      });
     });
   });
 });
