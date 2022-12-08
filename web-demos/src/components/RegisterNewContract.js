4517303eb9a18438b886768a6f9c07cecae59349
Thiago Cerutti <thiagocerutti@gmail.com>
2022-12-07 22:02:15 -0300

    wip: call smart contract from JS

--- web-demos/src/components/RegisterNewContract.js
+++ web-demos/src/components/RegisterNewContract.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 const sha256 = require("js-sha256");
+const contractAbi =
+  require("../contract-interfaces/SalesRegistration.json").abi;
 
 const RegisterNewContract = () => {
   const sampleContractValue = `Comprador: John Smith (CPF: 000.000.000-00); Vendedor: Lucy Brown (CPF: 111.111.111-11); Imóvel: apartamento na Rua Something, número 42, Bairro Centro, área construída 350m²; Valor de negociação: R$ 750.000,00; Data atual: ${new Date().toISOString()}`;
@@ -14,6 +16,21 @@ const RegisterNewContract = () => {
   useEffect(() => {
     setGeneratedSha256Hash(sha256(contractOriginalContent));
   }, [contractOriginalContent]);
+
+  const onClickTest = () => {
+    const Web3 = require("web3");
+    const web3 = new Web3("http://localhost:7545");
+
+    const contractOwner = "0xD98D99D5372431cBa43E6aA27925861B2131C5BB";
+    const contractAddress = "0xF2641343E730085f778D070F555b85289f7389ce";
+    const myContract = new web3.eth.Contract(contractAbi, contractAddress);
+    myContract.methods.getContractDetails(contractOwner).call((err, res) => {
+      if (err) {
+        console.log("error", err);
+      }
+      console.log("success", res);
+    });
+  };
 
   const onClickRegisterContract = async () => {
     let account = await window.ethereum.request({
@@ -32,6 +49,7 @@ const RegisterNewContract = () => {
   return (
     <div>
       <h1>New Contract Registration</h1>
+      <button onClick={onClickTest}>test me!</button>
       <div>
         <h2>Contract original content</h2>
         <textarea
